8ee4de19fd16b02c0ef2b397e5e081bfdd179d33
Vincent <jouanne.vincent@gmail.com>
2022-03-15 20:51:06 +0100

    :white_check_mark: (login) add failing case

--- flutter_bloc_clean_architecture/lib/dependency_container.dart
+++ flutter_bloc_clean_architecture/lib/dependency_container.dart
@@ -1,22 +1,32 @@
+import 'dart:io';
+
 import 'package:flutter_bloc_clean_architecture/features/authentication/login/bloc/login_cubit.dart';
 import 'package:flutter_bloc_clean_architecture/features/authentication/login/use_cases/login_with_email_and_password_usecase.dart';
 import 'package:flutter_bloc_clean_architecture/features/authentication/login/use_cases/login_with_google_usecase.dart';
 import 'package:flutter_bloc_clean_architecture/features/authentication/signup/bloc/signup_cubit.dart';
 import 'package:flutter_bloc_clean_architecture/features/authentication/signup/use_cases/signup_usecase.dart';
 import 'package:flutter_bloc_clean_architecture/infrastructure/authentication/adapters/fake/in_memory_authentication_gateway.dart';
+import 'package:flutter_bloc_clean_architecture/infrastructure/authentication/adapters/fake/mock_authentication_gateway.dart';
 import 'package:flutter_bloc_clean_architecture/infrastructure/authentication/adapters/real/firebase_authentication_gateway.dart';
 import 'package:flutter_bloc_clean_architecture/infrastructure/authentication/port/authentication_gateway.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
-resolveDependencies({bool isTesting = false}) async {
+resolveDependencies(
+    {MockAuthenticationGateway? mockAuthenticationGateway}) async {
+  final isTesting = Platform.environment.containsKey('FLUTTER_TEST');
+
   // Infrastructure
-  final authenticationGateway = isTesting
-      ? InMemoryAuthenticationGateway()
-      : FirebaseAuthenticationGateway();
+  final authenticationGateway = _resolveDependencie<AuthenticationGateway>(
+      isTesting,
+      mockAuthenticationGateway,
+      InMemoryAuthenticationGateway(),
+      isTesting ? null : FirebaseAuthenticationGateway());
+
   await authenticationGateway.user.first;
-  getIt.registerSingleton<AuthenticationGateway>(authenticationGateway);
+  getIt.registerLazySingleton<AuthenticationGateway>(
+      () => authenticationGateway);
 
   // Use-cases
   getIt.registerSingleton<SignUpUseCase>(SignUpUseCase(getIt()));
@@ -28,4 +38,16 @@ resolveDependencies({bool isTesting = false}) async {
   // Blocs
   getIt.registerFactory<SignUpCubit>(() => SignUpCubit(getIt()));
   getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt(), getIt()));
+}
+
+T _resolveDependencie<T>(bool isTesting, T? mock, T? fake, T? real) {
+  if (isTesting && mock != null) {
+    return mock;
+  }
+
+  if (isTesting && fake != null) {
+    return fake;
+  }
+
+  return real!;
 }
--- flutter_bloc_clean_architecture/test/features/authentication/login/ui/login_page_test.dart
+++ flutter_bloc_clean_architecture/test/features/authentication/login/ui/login_page_test.dart
@@ -2,66 +2,124 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc_clean_architecture/dependency_container.dart';
 import 'package:flutter_bloc_clean_architecture/features/authentication/login/ui/login_page.dart';
 import 'package:flutter_bloc_clean_architecture/features/home/ui/home_page.dart';
+import 'package:flutter_bloc_clean_architecture/infrastructure/authentication/adapters/fake/mock_authentication_gateway.dart';
+import 'package:flutter_bloc_clean_architecture/infrastructure/authentication/adapters/models/user.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../../../../helpers/pump_app.dart';
 
 void main() {
   group('$LoginPage', () {
-    setUpAll(() {
-      resolveDependencies(isTesting: true);
+    group('Success cases', () {
+      setUpAll(() {
+        getIt.pushNewScope();
+        resolveDependencies();
+      });
+      tearDownAll(() => getIt.popScope());
+
+      testWidgets(
+          '\n GIVEN the user is on the login page'
+          '\n WHEN he enters a badly formatted email'
+          '\n THEN he should see an error message', (tester) async {
+        // GIVEN the user is on the login page
+        await tester.pumpWidget(const MaterialApp(home: LoginPage()));
+        expect(find.byType(LoginPage), findsOneWidget);
+
+        const invalidEmail = 'invalid-email';
+
+        final emailInput =
+            find.byKey(const Key('loginForm_emailInput_textField'));
+
+        // WHEN he enters a badly formatted email
+        await tester.enterText(emailInput, invalidEmail);
+        await tester.pump();
+
+        // THEN he should see an error message
+        expect(find.text('invalid email'), findsOneWidget);
+      });
+
+      testWidgets(
+          '\n GIVEN the user is on the login page and has entered a correct email'
+          '\n WHEN he enters a badly formatted password'
+          '\n THEN he should see an error message', (tester) async {
+        // GIVEN the user is on the login page and has entered a correct email
+        await tester.pumpWidget(const MaterialApp(home: LoginPage()));
+        expect(find.byType(LoginPage), findsOneWidget);
+
+        const validEmail = 'valid@gmail.com';
+        const invalidPassword = 'invalid-password';
+        final emailInput =
+            find.byKey(const Key('loginForm_emailInput_textField'));
+
+        final passwordInput =
+            find.byKey(const Key('loginForm_passwordInput_textField'));
+        await tester.enterText(emailInput, validEmail);
+
+        // WHEN he enters a badly formatted password
+        await tester.enterText(passwordInput, invalidPassword);
+        await tester.pump();
+
+        // THEN he should see an error message
+        expect(find.text('invalid password'), findsOneWidget);
+      });
+
+      testWidgets(
+          '\n GIVEN an existing user on the login page who has entered well formatted email and password'
+          '\n WHEN he clicks on login'
+          '\n THEN he should be redirected to the home page', (tester) async {
+        // GIVEN an existing user on the login page who has entered well formatted email and password
+        await tester.pumpApp(builder: (child) => const LoginPage());
+        expect(find.byType(LoginPage), findsOneWidget);
+
+        const validEmail = 'valid@gmail.com';
+        const valisPassword = 'Password123';
+        final emailInput =
+            find.byKey(const Key('loginForm_emailInput_textField'));
+        final passwordInput =
+            find.byKey(const Key('loginForm_passwordInput_textField'));
+
+        await tester.enterText(emailInput, validEmail);
+        await tester.enterText(passwordInput, valisPassword);
+        await tester.pump();
+
+        // WHEN he clicks on login
+        await tester
+            .tap(find.byKey(const Key("loginForm_continue_raisedButton")));
+
+        await tester.pumpAndSettle();
+
+        // THEN he should be redirected to the home pag
+        expect(find.byType(HomePage), findsOneWidget);
+      });
     });
-    testWidgets(
-        'GIVEN the user is on the login page'
-        'WHEN he enters an invalid email'
-        'THEN he should see an error message', (tester) async {
-      //GIVEN the user is on the login page
-      await tester.pumpWidget(const MaterialApp(home: LoginPage()));
-      expect(find.byType(LoginPage), findsOneWidget);
+  });
 
-      const invalidEmail = 'invalid-email';
+  group('Failing cases', () {
+    setUp(() {
+      getIt.pushNewScope();
 
-      final emailInput =
-          find.byKey(const Key('loginForm_emailInput_textField'));
+      final _mockAuthenticationGateway = MockAuthenticationGateway();
+      when(() => _mockAuthenticationGateway.logInWithEmailAndPassword(
+          email: 'valid@gmail.com',
+          password: 'Password123')).thenAnswer((_) async => throw Exception());
 
-      //WHEN he enters an invalid email
-      await tester.enterText(emailInput, invalidEmail);
-      await tester.pump();
+      when(() => _mockAuthenticationGateway.user)
+          .thenAnswer((_) => Stream.value(User.empty));
 
-      //THEN he should see an error message
-      expect(find.text('invalid email'), findsOneWidget);
+      when(() => _mockAuthenticationGateway.currentUser)
+          .thenAnswer((_) => User.empty);
+
+      resolveDependencies(
+          mockAuthenticationGateway: _mockAuthenticationGateway);
     });
 
     testWidgets(
-        'GIVEN the user is on the login page and has entered a correct email'
-        'WHEN he enters an invalid password'
-        'THEN he should see an error message', (tester) async {
-      //GIVEN the user is on the login page and has entered a correct email'
-      await tester.pumpWidget(const MaterialApp(home: LoginPage()));
-      expect(find.byType(LoginPage), findsOneWidget);
-
-      const validEmail = 'valid@gmail.com';
-      const invalidPassword = 'invalid-password';
-      final emailInput =
-          find.byKey(const Key('loginForm_emailInput_textField'));
-
-      final passwordInput =
-          find.byKey(const Key('loginForm_passwordInput_textField'));
-      await tester.enterText(emailInput, validEmail);
-
-      //WHEN he enters an invalid password
-      await tester.enterText(passwordInput, invalidPassword);
-      await tester.pump();
-
-      //THEN he should see an error message
-      expect(find.text('invalid password'), findsOneWidget);
-    });
-
-    testWidgets(
-        'GIVEN the user is on the login page and has entered a correct email and password'
-        'WHEN he clicks on login'
-        'THEN he should be redirected to the home page', (tester) async {
-      //GIVEN the user is on the login page and has entered a correct email'
+        '\n GIVEN an unknown user on the login page who has entered well formatted email and password'
+        '\n WHEN he clicks on login'
+        '\n THEN he should not be redirected to the home page and see an error message',
+        (tester) async {
+      // GIVEN an unknown user on the login page who has entered well formatted email and password
       await tester.pumpApp(builder: (child) => const LoginPage());
       expect(find.byType(LoginPage), findsOneWidget);
 
@@ -76,14 +134,15 @@ void main() {
       await tester.enterText(passwordInput, valisPassword);
       await tester.pump();
 
-      //WHEN he taps on login button
+      // WHEN he clicks on login
       await tester
           .tap(find.byKey(const Key("loginForm_continue_raisedButton")));
 
       await tester.pumpAndSettle();
 
-      //THEN he should be redirected to the home page
-      expect(find.byType(HomePage), findsOneWidget);
+      // THEN he should not be redirected to the home page and see an error message
+      expect(find.byType(HomePage), findsNothing);
+      expect(find.text("Authentication Failure"), findsOneWidget);
     });
   });
 }
