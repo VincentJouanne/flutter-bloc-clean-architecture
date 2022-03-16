7aaaab7d6f045b9058727b1b3f713111729aac43
Vincent <jouanne.vincent@gmail.com>
2022-03-17 00:09:16 +0100

    :white_check_mark: (cache) add unit tests

---
+++ flutter_bloc_clean_architecture/test/core/cache/cache_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_bloc_clean_architecture/core/cache/cache.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('$CacheClient', () {
+    test('should read value if written', () {
+      final cache = CacheClient()..write(key: '1', value: 'a');
+      expect(cache.read(key: '1'), 'a');
+    });
+
+    test('should not read value if written but deleted', () {
+      final cache = CacheClient()
+        ..write(key: '1', value: 'a')
+        ..remove(key: '1');
+      expect(cache.read(key: '1'), null);
+    });
+  });
+}
