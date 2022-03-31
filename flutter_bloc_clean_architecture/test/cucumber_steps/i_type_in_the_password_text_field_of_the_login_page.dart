d513f0c4ed2df23d68cc29a3c257d8cb81e992bf
Vincent <jouanne.vincent@gmail.com>
2022-03-31 09:39:21 +0200

    :art: (lint) add trailing commas

--- flutter_bloc_clean_architecture/test/cucumber_steps/i_type_in_the_password_text_field_of_the_login_page.dart
+++ flutter_bloc_clean_architecture/test/cucumber_steps/i_type_in_the_password_text_field_of_the_login_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 Future<void> iTypeInThePasswordTextFieldOfTheLoginPage(
-    WidgetTester tester, String password) async {
+  WidgetTester tester,
+  String password,
+) async {
   final passwordInput =
       find.byKey(const Key('loginForm_passwordInput_textField'));
   await tester.enterText(passwordInput, password);
