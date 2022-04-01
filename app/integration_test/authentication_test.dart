e94413505b8ae1c2459d8ccb62856c1c85146569
Vincent <jouanne.vincent@gmail.com>
2022-04-01 20:38:55 +0200

    :construction: (e2e) authentication

--- app/integration_test/authentication_test.dart
+++ app/integration_test/authentication_test.dart
@@ -3,32 +3,35 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
-import './../test/cucumber_steps/the_dependencies_are_resolved_with_an_error_when_signing_up.dart';
+import './../test/cucumber_steps/the_dependencies_are_resolved_and_the_app_is_running_on_a_device.dart';
 import './../test/cucumber_steps/the_dependencies_are_popped.dart';
 import './../test/cucumber_steps/i_am_on_the_signup_page.dart';
 import './../test/cucumber_steps/i_type_in_the_email_text_field_of_the_signup_page.dart';
 import './../test/cucumber_steps/i_type_in_the_password_text_field_of_the_signup_page.dart';
 import './../test/cucumber_steps/i_type_in_the_confirmed_password_text_field_of_the_signup_page.dart';
 import './../test/cucumber_steps/i_tap_on_the_signup_button.dart';
-import './../test/cucumber_steps/i_should_see_a_text.dart';
+import './../test/cucumber_steps/i_should_be_redirected_to_the_home_page.dart';
 
 void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
   Future<void> bddSetUp(WidgetTester tester) async {
-    await theDependenciesAreResolvedWithAnErrorWhenSigningUp(tester);
+    await theDependenciesAreResolvedAndTheAppIsRunningOnADevice(tester);
   }
   Future<void> bddTearDown(WidgetTester tester) async {
     await theDependenciesArePopped(tester);
   }
-  group('''Sign up fails''', () {
-    testWidgets('''An unknown user has entered valid credentials but his mail address has beed disabled''', (tester) async {
+  group('''Authentication''', () {
+    testWidgets('''signup''', (tester) async {
       await bddSetUp(tester);
       await iAmOnTheSignupPage(tester);
       await iTypeInTheEmailTextFieldOfTheSignupPage(tester, 'vincent@gmail.com');
       await iTypeInThePasswordTextFieldOfTheSignupPage(tester, 'Password123');
       await iTypeInTheConfirmedPasswordTextFieldOfTheSignupPage(tester, 'Password123');
       await iTapOnTheSignupButton(tester);
-      await iShouldSeeAText(tester, 'This user has been disabled. Please contact support for help.');
+      await iShouldBeRedirectedToTheHomePage(tester);
       await bddTearDown(tester);
     });
   });
--- app/test/cucumber_steps/i_should_see_two_texts.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-Future<void> iShouldSeeTwoTexts(WidgetTester tester, dynamic param1, dynamic param2) async {
-  throw UnimplementedError();
-}
