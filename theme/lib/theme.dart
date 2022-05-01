5414cf038515876baed3709fa99c86abf61584e7
Vincent <jouanne.vincent@gmail.com>
2022-05-01 20:02:33 +0200

    :lipstick: (input) add primary inputs

--- theme/lib/theme.dart
+++ theme/lib/theme.dart
@@ -3,4 +3,5 @@ library theme;
 export 'src/data/theme_data_container.dart';
 export 'src/theme_resolver.dart';
 export 'src/widgets/app_button.dart';
+export 'src/widgets/app_input.dart';
 export 'src/widgets/app_text.dart';
